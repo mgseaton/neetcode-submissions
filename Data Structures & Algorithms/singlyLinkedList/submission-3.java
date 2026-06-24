db7ecf03b30837804605a2dd1ff570097ae7c00e
Marigrace Seaton <marigraceseaton@gmail.com>
2026-06-24 09:13:18 -0400

    Add: singlyLinkedList - submission-3

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-3.java
@@ -0,0 +1,87 @@
+class LinkedList {
+
+    private ListNode head; 
+
+    public LinkedList() {
+        this.head = new ListNode(null); 
+    }
+
+    public int get(int index) {
+        int currIndex = 0; 
+        ListNode currNode = this.head.getNext(); 
+        while (currNode != null) {
+            if (currIndex == index) return currNode.getVal(); 
+            currNode = currNode.getNext(); 
+            currIndex++; 
+        }
+        return -1; 
+    }
+
+    public void insertHead(int val) {
+        ListNode currHead = this.head.getNext(); 
+        ListNode newHead = new ListNode(val); 
+        newHead.setNext(currHead); 
+        this.head.setNext(newHead); 
+    }
+
+    public void insertTail(int val) {
+        ListNode currNode = this.head; 
+        while (currNode.hasNext()) {
+            currNode = currNode.getNext(); 
+        }
+        currNode.setNext(new ListNode(val)); 
+    }
+
+    public boolean remove(int index) {
+        int currIndex = 0; 
+        ListNode prevNode = this.head, currNode = this.head.getNext(); 
+        while (currNode != null) {
+            if (currIndex == index) {
+                prevNode.setNext(currNode.getNext()); 
+                return true; 
+            }
+            prevNode = prevNode.getNext(); 
+            currNode = currNode.getNext(); 
+            currIndex++; 
+        }
+        return false; 
+    }
+
+    public ArrayList<Integer> getValues() {
+        ArrayList<Integer> res = new ArrayList<>();
+        ListNode curr = this.head.next;
+        while (curr != null) {
+            res.add(curr.val);
+            curr = curr.next;
+        }
+        return res;
+    }
+}
+
+class ListNode {
+
+    private ListNode next; 
+    private Integer val; 
+
+    public ListNode(Integer val) {
+        this.val = val;
+        this.next = null;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next; 
+    }
+
+    public boolean hasNext() {
+        return this.next != null; 
+    }
+
+    public int getVal() {
+        return (this.val == null) ? 0 : this.val; 
+    }
+
+    public ListNode getNext() {
+        return this.next; 
+    }
+
+}
